9f9db1336613187aeaf43a11cf3308600b7c510d
edercova <111379409+edercova@users.noreply.github.com>
2025-12-18 19:54:46 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
 
+    if (loginForm) {
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault(); // Evita que la página se recargue
+
+            const email = document.getElementById('email').value;
+            const pass = document.getElementById('password').value;
+
+            // Validación de prueba para la directiva
+            if (email === "director@colegio.com" && pass === "agustin2024") {
+                window.location.href = "dashboard.html";
+            } else {
+                alert("Usuario o clave incorrecta. Por favor use las credenciales de prueba que aparecen abajo.");
+            }
+        });
+    }
+});
